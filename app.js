a0c8afd8ea823087fbeb28f3f1a6123052057c22
brian-quach <briiquach@gmail.com>
2018-07-11 22:26:14 -0400

    k

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
-// Load up the discord.js library
+// discord.js library
 const Discord = require("discord.js");
 
-// This is your client. Some people call it `bot`, some people call it `self`,
-// some might call it `cootchie`. Either way, when you see `client.something`, or `bot.something`,
-// this is what we're refering to. Your client.
+// Using NeDB b/c lazy
+var Datastore = require('nedb');
+
+// Client, database, etc.
 const client = new Discord.Client();
+var users = new Datastore({ filename: 'db/users.db', autoload: true });
+var prevcmd = new Datastore();
 
 
 const config = require("./config.json");
 // config.token contains the bot's token
 // config.prefix contains the message prefix.
+// config.status contains bot's status message
 
 client.on("ready", () => {
-    // This event will run if the bot starts, and logs in, successfully.
-    console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
+    // Log something when bot starts
+    console.log(`rdy2go!!!`);
 
     client.user.setActivity(`wekwek`);
 });
