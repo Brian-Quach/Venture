b5aec72a83f5fb9bb82849255d7ee743e87c8c0b
brian-quach <briiquach@gmail.com>
2018-07-20 16:31:38 -0400

    Added currency, give/take commands

--- app.js
+++ app.js
@@ -2,14 +2,7 @@
 const Discord = require("discord.js");
 const Gamble = require("./gamble");
 
-// Using NeDB b/c lazy
-var Datastore = require('nedb');
-
-// Client, database, etc.
 const client = new Discord.Client();
-var users = new Datastore({ filename: 'db/users.db', autoload: true });
-var prevcmd = new Datastore();
-
 
 const config = require("./config.json");
 // config.token contains the bot's token
@@ -25,11 +18,17 @@ client.on("ready", () => {
 
 client.on("message", async message => {
 
+    var userId = "";
+
     // Ignore bot messages
     if(message.author.bot) return;
 
     // Checks for prefix
-    if(message.content.indexOf(config.prefix) !== 0) return;
+    if(message.content.indexOf(config.prefix) !== 0) {
+        return;
+    } else {
+        userId = message.author.id;
+    }
 
     // Separate commands and args
     // args -> ["List", "of", "args"]
@@ -37,22 +36,90 @@ client.on("message", async message => {
     const command = args.shift().toLowerCase();
 
     if (["help", "h"].indexOf(command) > -1){
-        var helpMsg =
-            "```css\n" +
-            "Commands:\n" +
-            "   wek: wekwek\n" +
-            "   help, h: Display all commands" +
-            "```";
-        message.channel.send(helpMsg);
-    } else if (command === 'wek') {
+        message.channel.send({embed: {
+                color: 3447003,
+                title: "Commands",
+                fields: [
+                    {
+                        name: "bal",
+                        value: "Display user balance."
+                    },
+                    {
+                        name: "income",
+                        value: "Use to redeem your income!"
+                    },
+                    {
+                        name: "flip",
+                        value: "Flip a 2-sided coin!"
+                    },
+                    {
+                        name: "help, h",
+                        value: "Display all commands"
+                    }
+                ]
+            }
+        });
+    } else if (command === "wek") {
         // Test if bot is connected, reply w/ "wekwek"
         message.channel.send('wekwek :penguin:');
+    } else if (command === "bal") {
+        let userBal;
+        if (!(await Gamble.userExists(userId))){
+            userBal = await Gamble.createUser(userId);
+        } else {
+            userBal = await Gamble.getBal(userId);
+        }
+        message.channel.send("$"+userBal);
+    } else if (command === "give"){
+        //TODO: check admin rights and shit
+
+        let amt = parseFloat(args.shift());
+        let user = message.mentions.members.first();
+
+        if (isNaN(amt) || (message.mentions.members.array().length !== 1)) {
+            message.channel.send("FormatErr - Use !give [amt] [user]");
+            return;
+        }
+
+        let userId = user.id;
+        if (!(await Gamble.userExists(userId))){
+            await Gamble.createUser(userId);
+        }
+
+        await Gamble.addBal(userId, amt);
+
+        message.channel.send("Gave $" + amt + " to <@" + userId + ">");
+
+    } else if (command === "take"){
+        //TODO: check admin rights and shit
+
+        let userBal;
+        let amt = parseFloat(args.shift());
+        let user = message.mentions.members.first();
+
+        if (isNaN(amt) || (message.mentions.members.array().length !== 1)) {
+            message.channel.send("FormatErr - Use !take [amt] [user]");
+            return;
+        }
+
+        let userId = user.id;
+        if (!(await Gamble.userExists(userId))){
+            userBal = await Gamble.createUser(userId);
+        } else {
+            userBal = await Gamble.getBal(userId);
+        }
+
+        if (userBal < amt){
+            message.channel.send("<@" + userId + "> does not have $" + amt + "!");
+        } else {
+            await Gamble.takeBal(userId, amt);
+            message.channel.send("Took $" + amt + " from <@" + userId + ">");
+        }
+
+
     } else {
         // Command not found, return error message
-        var errMsg =
-            "```css\n" +
-            "Command not found, use !help to see commands" +
-            "```";
+        var errMsg = {embed: {color: 3447003, description: "Command not found, use !help to see commands"}};
         message.channel.send(errMsg);
     }
 });
--- gamble.js
+++ gamble.js
@@ -1,14 +1,99 @@
 //gamble.js
+// Using NeDB b/c lazy
+var Datastore = require('nedb');
+
+var users = new Datastore({ filename: 'db/users.db', autoload: true });
+var prevcmd = new Datastore();
+
+var User = (function(){
+    return function item(id){
+        this._id = id;
+        this.bal = 10000;
+    };
+}());
+
 module.exports = {
-    createUser: function() {
-        return 0;
+    currentTime: function() {
+        return new Date().getTime();
+        //this.currentTime(); to use
     },
 
-    userExists: function() {
-        return true;
+    createUser: function(userId) {
+        return new Promise(function(resolve, reject) {
+            let newUser = new User(userId);
+            users.insert(newUser, function (err, newUsr){
+                if (err) {
+                    reject(err);
+                }
+                else {
+                    resolve(newUsr.bal);
+                }
+            });
+        });
+
     },
 
-    getBal: function() {
-        return "Hello";
+    userExists: function(userId) {
+        return new Promise(function(resolve, reject){
+            users.count({_id: userId}, function (err, user) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(user === 1);
+                }
+            })
+        });
+    },
+
+    getBal: function(userId) {
+        return new Promise(function(resolve, reject){
+            users.findOne({ _id: userId }, function (err, user) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(user.bal);
+                }
+            });
+        })
+    },
+
+    addBal: function(userId, amt) {
+        return new Promise(function(resolve, reject){
+            users.findOne({ _id: userId }, function (err, user) {
+                if (err) {
+                    reject(err);
+                } else {
+                    let newAmt = user.bal + amt;
+                    users.update({ _id: userId }, { $set: { bal: newAmt } }, function (err, rep) {
+                        if (err) {
+                            reject(err);
+                        } else {
+                            resolve();
+                        }
+                    });
+                }
+            });
+
+        })
+    },
+
+    takeBal: function(userId, amt) {
+        return new Promise(function(resolve, reject){
+            users.findOne({ _id: userId }, function (err, user) {
+                if (err) {
+                    reject(err);
+                } else {
+                    let newAmt = Math.max(user.bal - amt, 0);
+                    users.update({ _id: userId }, { $set: { bal: newAmt } }, function (err, rep) {
+                        if (err) {
+                            reject(err);
+                        } else {
+                            resolve();
+                        }
+                    });
+                }
+            });
+
+        })
     }
 };
